71d62e8b873adfa982d079642ef0c3804fe726c8
Vikash <vikash.my022@gmail.com>
2026-05-10 14:09:54 +0530

    fixed missing upload. api issue

---
+++ frontend/src/api/upload.api.js
@@ -0,0 +1,14 @@
+import axiosInstance from './axios';
+
+export const uploadImage = async (file) => {
+  const formData = new FormData();
+  formData.append('image', file);
+  
+  const response = await axiosInstance.post('/upload', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+  
+  return response.data.data;
+};
